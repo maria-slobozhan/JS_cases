bb944cbcaa4c9723f268ec3e67c79ff21b7d6fba
maria-slobozhan <slobozhan.mi@gmail.com>
2021-05-27 00:09:29 +0300

    15 задач первого модуля

---
+++ module1.js
@@ -0,0 +1,77 @@
+// Задание 11
+// Функция calculateTotalPrice считает и возвращает общую сумму покупки. Она принимает два параметра, значения которых будут задаваться во время её вызова.
+
+// orderedQuantity - количество единиц товара в заказе;
+// pricePerItem - цена одной единицы товара.
+// Дополни код функции так, чтобы в переменную totalPrice записывалась общая сумма покупки, результат умножения кол-ва товаров на цену одного.
+
+
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+    
+    const totalPrice = orderedQuantity * pricePerItem;
+    return totalPrice;
+};
+
+
+// Задание 12
+// Функция makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) составляет и возвращает сообщение о покупке ремонтных дроидов. Она объявляет три параметра, значения которых будут задаваться во время её вызова.
+
+// orderedQuantity - количество дроидов в заказе
+// pricePerDroid - цена одного дроида
+// deliveryFee - стоимость доставки
+// Дополни код функции так, чтобы она возвращала сообщение о заказе в формате "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь о цене доставки при вычислениях общей стоимости.
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  
+    const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+    const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+    
+  return message;
+};
+
+
+// Задача 13
+// Функция isAdult объявляет один параметр age (возраст), значение которого будет задаваться во время её вызова. 
+// Присвой переменной passed выражение проверки возраста пользователя на совершеннолетие.
+// Человек считается совершеннолетним в возрасте 18 лет и старше.
+
+function isAdult(age) {
+  // Change code below this line
+  const passed = age >= 18;
+
+  // Change code above this line
+  return passed;
+};
+
+// Задача 14
+// Функция isValidPassword(password) проверяет равенство сохранённого и введённого паролей и возвращает результат проверки - буль true или false.
+// Переменная SAVED_PASSWORD хранит значение ранее сохраненного пароля.Введённый пароль передаётся в параметр password.
+// Присвой переменной isMatch выражение проверки равенства введённого и сохранённого ранее паролей.
+// Результатом выражения проверки должно быть true, если значения совпадают, и false, если нет.
+
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  const isMatch = password === SAVED_PASSWORD;
+
+  // Change code above this line
+  return isMatch;
+};
+
+// Задача 15
+// Добавь выражение проверки совершеннолетия пользователя, значения параметра age, в условие для инструкции if.
+
+// Если пользователь совершеннолетний, должен выполняться блок if и в переменную message записывается строка "You are an adult".
+// В противном случае должен выполняться блок else и записывается строка "You are a minor".
+
+function checkAge(age) {
+  let message;
+
+  if (age >= 18) { // Change this line
+    message = 'You are an adult';
+  } else {
+    message = 'You are a minor';
+  }
+
+  return message;
+};
