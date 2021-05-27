c89c55c6ec8ffe8b0ef89780dfa6edb618eccf41
maria-slobozhan <slobozhan.mi@gmail.com>
2021-05-28 01:05:55 +0300

    24 домашки

--- module1.js
+++ module1.js
@@ -6,11 +6,11 @@
 // Дополни код функции так, чтобы в переменную totalPrice записывалась общая сумма покупки, результат умножения кол-ва товаров на цену одного.
 
 
-function calculateTotalPrice (orderedQuantity, pricePerItem) {
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
     
-    const totalPrice = orderedQuantity * pricePerItem;
-    return totalPrice;
-};
+//     const totalPrice = orderedQuantity * pricePerItem;
+//     return totalPrice;
+// };
 
 
 // Задание 12
@@ -21,13 +21,13 @@ function calculateTotalPrice (orderedQuantity, pricePerItem) {
 // deliveryFee - стоимость доставки
 // Дополни код функции так, чтобы она возвращала сообщение о заказе в формате "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь о цене доставки при вычислениях общей стоимости.
 
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
   
-    const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
-    const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//     const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//     const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
     
-  return message;
-};
+//   return message;
+// };
 
 
 // Задача 13
@@ -35,13 +35,13 @@ function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
 // Присвой переменной passed выражение проверки возраста пользователя на совершеннолетие.
 // Человек считается совершеннолетним в возрасте 18 лет и старше.
 
-function isAdult(age) {
-  // Change code below this line
-  const passed = age >= 18;
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
 
-  // Change code above this line
-  return passed;
-};
+//   // Change code above this line
+//   return passed;
+// };
 
 // Задача 14
 // Функция isValidPassword(password) проверяет равенство сохранённого и введённого паролей и возвращает результат проверки - буль true или false.
@@ -49,14 +49,14 @@ function isAdult(age) {
 // Присвой переменной isMatch выражение проверки равенства введённого и сохранённого ранее паролей.
 // Результатом выражения проверки должно быть true, если значения совпадают, и false, если нет.
 
-function isValidPassword(password) {
-  const SAVED_PASSWORD = 'jqueryismyjam';
-  // Change code below this line
-  const isMatch = password === SAVED_PASSWORD;
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+//   const isMatch = password === SAVED_PASSWORD;
 
-  // Change code above this line
-  return isMatch;
-};
+//   // Change code above this line
+//   return isMatch;
+// };
 
 // Задача 15
 // Добавь выражение проверки совершеннолетия пользователя, значения параметра age, в условие для инструкции if.
@@ -64,14 +64,211 @@ function isValidPassword(password) {
 // Если пользователь совершеннолетний, должен выполняться блок if и в переменную message записывается строка "You are an adult".
 // В противном случае должен выполняться блок else и записывается строка "You are a minor".
 
-function checkAge(age) {
-  let message;
+// function checkAge(age) {
+//   let message;
 
-  if (age >= 18) { // Change this line
-    message = 'You are an adult';
-  } else {
-    message = 'You are a minor';
+//   if (age >= 18) { // Change this line
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+
+//   return message;
+// };
+
+// Задание 18
+// Станция по продаже ремонтных дроидов готова к запуску, осталось написать программное обеспечение для отдела продаж.
+
+// Функция makeTransaction(pricePerDroid, orderedQuantity, customerCredits) выполняет транзакцию по продаже дроидов и возвращает сообщение о результате операции.
+// Она объявляет три параметра, значения которых будут задаваться во время её вызова:
+
+// pricePerDroid - цена одного дроида
+// orderedQuantity - кол-во заказанных дроидов
+// customerCredits - сумма средств на счету клиента
+// Дополни её следующим функционалом:
+
+// Объяви переменную totalPrice для хранения общей суммы заказа и присвой ей выражение расчёта этой суммы.
+// Добавь проверку сможет ли клиент оплатить заказ:
+// если сумма к оплате превышает количество кредитов на счету клиента, запиши в переменную message строку "Insufficient funds!";
+// в противном случае, вычти сумму покупки со счёта клиента и запиши в переменную message сообщение: "You ordered <число> droids, you have <число> credits left".
+// Тесты
+// Объявлена функция makeTransaction(pricePerDroid, orderedQuantity, customerCredits)
+// Вызов makeTransaction(3000, 5, 23000) возвращает "You ordered 5 droids, you have 8000 credits left"
+// Вызов makeTransaction(1000, 3, 15000) возвращает "You ordered 3 droids, you have 12000 credits left"
+// Вызов makeTransaction(5000, 10, 8000) возвращает "Insufficient funds!"
+// Вызов makeTransaction(2000, 8, 10000) возвращает "Insufficient funds!"
+// Вызов makeTransaction(500, 10, 5000) возвращает "You ordered 10 droids, you have 0 credits left"
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+  
+//   if(totalPrice <= customerCredits) {
+//     customerCredits -= totalPrice;
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;;
+//   } else {
+//     message = `Insufficient funds!`;
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// const result = makeTransaction(3000, 5, 23000);
+// console.log(result)
+
+
+// Задание 19
+// Функция checkPassword(password) получает пароль пользователя в параметр password,
+//   проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и возвращает сообщение о результате сравнения,
+//     хранящееся в переменной message.
+
+// Если значение параметра password равно null, значит пользователь отменил операцию и в message записывается строка "Canceled by user!".
+// Если значение параметра password совпадает со значением ADMIN_PASSWORD, в переменную message присваивается строка "Welcome!".
+// Если ни одно из предыдущих условий не выполнилось, в переменную message записывается строка "Access denied, wrong password!".
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === null) { 
+//     message =  'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) { 
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+
+//   return message;
+// }
+
+// const result = checkPassword('jqueryismyjam');
+
+// console.log(result)
+
+
+// Задание 20
+// Функция checkStorage(available, ordered) проверяет возможность оформления заказа и возвращает сообщение о результате. Она объявляет два параметра, значения которых будут задаваться во время её вызова.
+
+// available - доступное количество товаров на складе
+// ordered - единиц товара в заказе
+// Используя ветвления дополни код функции так, что:
+
+// Если в заказе еще нет товаров, то есть значение параметра ordered равно 0, в переменную message присваивается строка "There are no products in the order!".
+// Eсли товаров в заказе больше чем доступно товаров на складе, то в переменную message присваивается строка "Your order is too large, there are not enough items in stock!".
+// В противном случае в переменную message присваевается строка "The order is accepted, our manager will contact you".
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   if(ordered === 0){
+//   message = `There are no products in the order!`;
+//   } else if (ordered > available) {
+//   message = `Your order is too large, there are not enough items in stock!`;
+//   } else {
+//   message = `The order is accepted, our manager will contact you`
+//   }
+//   return message;
+// }
+
+// const result = checkStorage(10, 2);
+
+// console.log(result)
+
+
+// Задание 21
+// Функция isNumberInRange(start, end, number) проверяет, входит ли число в промежуток.
+// Она объявляет три параметра, значения которых будут задаваться во время её вызова:
+
+// number - число, вхождение которого проверяется
+// start - начало числового промежутка
+// end - конец числового промежутка
+// Присвой переменной isInRange выражение проверки вхождения number в числовой промеждуток от start до end.
+// То есть число должно быть больше либо равно start и меньше либо равно end.
+// Результатом выражения проверки будет буль true или false.
+
+
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end; // Change this line
+
+//   return isInRange;
+// }
+
+// const result = isNumberInRange(2,50,30);
+
+// console.log(result)
+
+// Задание 22
+// Функция checkIfCanAccessContent(subType) проверяет, может ли пользователь получить доступ к контенту.
+// Проверка происходит по типу подписки.Получить доступ могут только пользователи с подпиской pro или vip.
+
+// Присвой переменной canAccessContent выражение проверки подписки.Если значение параметра subType равно строкам "pro" или "vip",
+//   пользователь получит доступ.
+// Результатом выражения проверки будет буль true или false.
+
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === "pro" || subType === "vip"; // Change this line
+
+//   return canAccessContent;
+// }
+
+// const result = checkIfCanAccessContent("vip");
+
+// console.log(result)
+
+// Задание 23
+// Функция isNumberNotInRange(start, end, number) проверяет, не входит ли число в промежуток. 
+// То есть число должно быть меньше либо равно start и больше либо равно end.Результатом выражения проверки будет буль true или false.
+
+// Она объявляет три параметра, значения которых будут задаваться во время её вызова:
+
+// number - число, не вхождение которого проверяется
+// start - начало числового промежутка
+// end - конец числового промежутка
+// Присвой переменной isNotInRange выражение инверсии значения переменной isInRange используя оператор !.
+
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange;
+
+//   return isNotInRange;
+// }
+
+// const result = isNumberNotInRange(10, 30, 5);
+
+// console.log(result)
+
+// Задание 24
+// Функция getDiscount(totalSpent) определяет значение скидки в зависимости от общей суммы потраченных денег(параметр totalSpent)
+// в магазине за всё время(партнёрская программа).Скидка записывается в переменную discount и возвращается из функции как результат её работы.
+
+// Используя ветвления и логические операторы, дополни код функции.
+
+// Если потрачено от 50000 ( включительно ) или больше кредитов - скидка 10% (золотой партнёр)
+// Если потрачено от 20000 (включительно) до 50000 кредитов - скидка 5% (серебрянный партнёр)
+// Если потрачено от 5000 (включительно) до 20000 кредитов - скидка 2% (бронзовый партнёр)
+// Если потрачено меньше чем 5000 кредитов - скидка 0 (базовый партнёр)
+// Значения скидок каждого уровня хранятся в одноимённых константах BASE_DISCOUNT, BRONZE_DISCOUNT, SILVER_DISCOUNT и GOLD_DISCOUNT.
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+
+  if (totalSpent >= 50000) {
+    discount = GOLD_DISCOUNT
   }
+  else if (totalSpent >= 20000 && totalSpent < 50000) {
+    discount = SILVER_DISCOUNT
+  }
+  else if (totalSpent >= 5000 && totalSpent < 20000) {
+    discount = BRONZE_DISCOUNT
+  } else { discount = BASE_DISCOUNT }
 
-  return message;
-};
+  return discount;
+}
+const result = getDiscount(137000);
+
+console.log(result)
